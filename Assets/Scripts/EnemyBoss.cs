c411595d13b13674f30805f4f187d70003a055ed
xdSp1noguz <113174561+xdSp1noguz@users.noreply.github.com>
2026-04-19 18:44:11 +0300

    create a 60% locations

---
+++ Assets/Scripts/EnemyBoss.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EnemyBoss : MonoBehaviour
+{
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
